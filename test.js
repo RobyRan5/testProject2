016edaffde1070b62641d45a04b9bc1c91d40b53
Roby Ran <robyran@icloud.com>
2020-02-05 20:56:36 -0600

    commit for the night

--- test.js
+++ test.js
@@ -9,41 +9,42 @@ puppeteer.launch({ headless: true }).then(async browser => {
     const page = await browser.newPage();
     await page.emulate(iPhonex);
 
-    var cat = 'Real+Estate'; 
-    var loc = 'Downtown%2C+Austin%2C+TX';
+    // var cat = 'Real+Estate'; 
+    // var loc = 'Downtown%2C+Austin%2C+TX';
     var offset = 0;
 
     // var titleBegin = 'mark-str%C3%BCb-str%C3%BCb-residential-austin-3';
 
     // open this page
 
-    for (var i = offset; i < 2000; i += 10) {
-        await page.goto(`https://m.yelp.com/search?find_desc=${cat}&find_loc=${loc}&start=${i}`);
+    // for (var i = offset; i < 2000; i += 10) {
+    //     await page.goto(`https://m.yelp.com/search?find_desc=${cat}&find_loc=${loc}&start=${i}`);
             
         // get some stuff from 
 
-        const innerText = await page.evaluate(() => $.map($('.u-text-subtle.phone-number'), x => x.innerText));
-        const innerText2 = await page.evaluate(() => $.map($('.title-beginning'), x => x.innerText));
+        // const innerText = await page.evaluate(() => $.map($('.u-text-subtle.phone-number'), x => x.innerText));
+        // const innerText2 = await page.evaluate(() => $.map($('.title-beginning'), x => x.innerText));
 
     
         // run thru WHOIS API for external link
     
 
         // save to datbase
-        console.log(innerText);
-        console.log(innerText2.slice(10));
+        // console.log(innerText);
+        // console.log(innerText2.slice(10));
 
- }
+//  }
     
-    // for (var i = offset; i < 2000; i += 10) {
 
-    // await page.goto(`https://m.yelp.com/biz/mark-str%C3%BCb-str%C3%BCb-residential-austin-3?osq=Real+Estate`);
+    for (var i = offset; i < 2000; i += 10) {
 
-    // const innerText = await page.evaluate(() => $.map($('.'), x =>innerText));
+    await page.goto(`https://m.yelp.com/biz/mark-str%C3%BCb-str%C3%BCb-residential-austin-3?osq=Real+Estate`);
 
-    // console.log(innerText.slice(10));
+    const innerText = await page.evaluate(() => $.map($('.IEZFH._29943._21q5r._1mhJo._1skgq'), x => x.getAttribute('href')));
 
-    // }
+    console.log(innerText.slice(10));
+
+    }
 
     // await page.screenshot({path: 'example.png'});
 
